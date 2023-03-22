5c79d70e964e2968f572b1dabdf6d3a8db942a80
lucahu004 <hu.luca@iisgalvanimi.edu.it>
2023-03-22 20:35:24 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { StringinterpolationComponent } from './stringinterpolation/stringinterpolation.component';
+import { EventbindingComponent } from './eventbinding/eventbinding.component';
+import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringinterpolationComponent,
+    EventbindingComponent,
+    TwowaybindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
